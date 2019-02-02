49e032c5fdd788cd8498dfe7ef01abb3ccbd4ae8
Michael Robbins <mdrobbins@mdrnetworks.com>
2019-02-02 12:27:37 -0500

    Add bootstrap css to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import configureStore from './store/configureStore';
