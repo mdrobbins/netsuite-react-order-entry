4e46f16ad97e667f8808d65ebee30b81e517be13
Michael Robbins <mdrobbins@mdrnetworks.com>
2019-02-03 13:49:34 -0500

    Set focus in search box
    Converted to class component so we can use lifecycle hooks.

--- src/components/CustomerSearchScreen/CustomerSearchScreen.js
+++ src/components/CustomerSearchScreen/CustomerSearchScreen.js
@@ -1,61 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Container, Row, Col, Form, Tabs, Tab } from 'react-bootstrap';
 import CustomerListTable from './CustomerListTable/CustomerListTable';
 import Button from "react-bootstrap/Button";
 import * as actions from '../../actions/customerActions';
 
-const CustomerSearchScreen = props => {
-  const onSearchTextChange = e => props.dispatch(actions.searchTextChanged(e.target.value));
-
-  const onSubmitForm = e => {
-    e.preventDefault();
-    props.dispatch(actions.searchCustomers(props.customer.searchText));
+class CustomerSearchScreen extends Component {
+  componentDidMount = () => {
+    this.searchField.focus();
   };
 
-  const onTabSelect = key => props.dispatch(actions.searchTabChanged(key));
+  componentDidUpdate = () => {
+    this.searchField.focus();
+  };
 
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <Form className="mt-4" onSubmit={onSubmitForm}>
+  render() {
+    const onSearchTextChange = e => this.props.dispatch(actions.searchTextChanged(e.target.value));
+
+    const onSubmitForm = e => {
+      e.preventDefault();
+      this.props.dispatch(actions.searchCustomers(this.props.customer.searchText));
+    };
+
+    const onTabSelect = key => this.props.dispatch(actions.searchTabChanged(key));
+
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Form className="mt-4" onSubmit={onSubmitForm}>
               <Row>
-                <Col md={{ offset: 2, span: 7}}>
+                <Col md={{ offset: 2, span: 7 }}>
                   <Form.Group controlId="customer-search" className="mt-1">
                     <Form.Label><h4>Customer Search</h4></Form.Label>
-                    <Form.Control value={props.customer.searchText}
-                                  onChange={onSearchTextChange}/>
+                    <Form.Control value={this.props.customer.searchText}
+                                  onChange={onSearchTextChange}
+                                  ref={input => this.searchField = input}/>
                   </Form.Group>
                 </Col>
                 <Col>
                   <Button variant="primary" className="mt-5" onClick={onSubmitForm}>Search</Button>
                 </Col>
               </Row>
-          </Form>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Tabs className="mt-5"
-                activeKey={props.customer.activeTab}
-                onSelect={key => onTabSelect(key)}>
-            <Tab eventKey="results"
-                 title="Search Results">
-              <CustomerListTable customers={props.customer.customerSearchResults}
-                                 message='No Search Results'/>
-            </Tab>
-            <Tab eventKey="recent"
-                 title="Recent Customers">
-              <CustomerListTable customers={props.customer.recentCustomers}
-                                 emptyMessage="No records found"/>
-            </Tab>
-          </Tabs>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Tabs className="mt-5"
+                  activeKey={this.props.customer.activeTab}
+                  onSelect={key => onTabSelect(key)}>
+              <Tab eventKey="results"
+                   title="Search Results">
+                <CustomerListTable customers={this.props.customer.customerSearchResults}
+                                   message='No Search Results'/>
+              </Tab>
+              <Tab eventKey="recent"
+                   title="Recent Customers">
+                <CustomerListTable customers={this.props.customer.recentCustomers}
+                                   emptyMessage="No records found"/>
+              </Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   customer: state.customer
