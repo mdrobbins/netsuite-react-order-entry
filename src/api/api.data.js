d24773a1912170b39ab9d39bbfb96ac7addc6082
Michael Robbins <mdrobbins@mdrnetworks.com>
2019-02-03 13:10:45 -0500

    Search all fields when searching for customers

--- src/api/api.data.js
+++ src/api/api.data.js
@@ -1,7 +1,12 @@
 import customerData from './data/customerData';
 
 export const searchCustomers = searchString => {
-  const filteredCustomers = customerData.filter(c => c.companyName.toLowerCase().includes(searchString));
+  const filteredCustomers = customerData.filter(c => {
+    return c.companyName.toLowerCase().includes(searchString)
+      || c.number.toLowerCase().includes(searchString)
+      || c.email.toLowerCase().includes(searchString)
+      || c.phone.replace(/\D/g, '').includes(searchString)
+  });
   return {
     isSuccess: true,
     result: filteredCustomers,
