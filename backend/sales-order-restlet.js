ebf7779a6a72a4635c9cad4c6a2b59c902d3b4a7
Michael Robbins <mdrobbins@mdrnetworks.com>
2019-02-12 19:24:18 -0500

    Add backend API for getCustomerDetail

--- backend/sales-order-restlet.js
+++ backend/sales-order-restlet.js
@@ -3,14 +3,26 @@
  * @NScriptType Restlet
  * @NModuleScope Public
  */
-define(['N/log', 'N/search', 'N/runtime', './safeExecute', './timer'],
-  function (log, search, runtime, safeExecute, Timer) {
+define(['N/log', 'N/search', 'N/runtime', 'N/util', './safeExecute', './timer'],
+  function (log, search, runtime, util, safeExecute, Timer) {
     function post(request) {
       var timer = new Timer();
       var response = {};
       switch (request.action) {
         case 'searchCustomers':
           response = safeExecute(getCustomers, request.searchText);
+          break;
+
+        case 'getCustomerDetail':
+          response = safeExecute(getCustomerDetail, request.customerId);
+          break;
+
+        case 'getCustomer':
+          response = safeExecute(getCustomer, request.customerId);
+          break;
+
+        case 'getCustomerOrders':
+          response = safeExecute(getCustomerOrders, request.customerId);
           break;
 
         default:
@@ -31,6 +43,80 @@ define(['N/log', 'N/search', 'N/runtime', './safeExecute', './timer'],
       return response;
 
       ////////////////////////////////////////
+
+      function getCustomerDetail(customerId) {
+        var customer = getCustomer(customerId);
+        customer.orders = getCustomerOrders(customerId);
+        return customer;
+      }
+
+      function getCustomerOrders(customerId) {
+        var results = search.create({
+          type: 'salesorder',
+          filters: [
+            ['mainline', 'is', 'T'],
+            'and', ['name', 'anyof', customerId]
+          ],
+          columns: [
+            'tranid',
+            'trandate',
+            'status',
+            'amount'
+          ]
+        }).run().getRange({ start: 0, end: 1000 });
+
+        return (results || []).map(function (result) {
+          return {
+            id: Number(result.id),
+            number: result.getValue('tranid'),
+            date: result.getValue('trandate'),
+            status: result.getText('status'),
+            total: Number(result.getValue('amount'))
+          };
+        });
+      }
+
+      function getCustomer(customerId) {
+        var results = search.create({
+          type: 'customer',
+          filters: [
+            ['isinactive', 'is', 'F'],
+            'and', ['internalid', 'anyof', customerId]
+          ],
+          columns: [
+            'entityid',
+            'companyname',
+            'email',
+            'phone',
+            'comments',
+            'balance',
+            'daysoverdue',
+            'address.internalid',
+            'address.addresslabel'
+          ]
+        }).run().getRange({ start: 0, end: 1000 });
+
+        return (results || []).reduce(function (acc, result) {
+          acc = util.extend(acc, {
+            id: Number(result.id),
+            number: result.getValue('entityid'),
+            companyName: result.getValue('companyname'),
+            email: result.getValue('email'),
+            phone: result.getValue('phone'),
+            comments: result.getValue('comments'),
+            balance: Number(result.getValue('balance')),
+            daysOverdue: Number(result.getValue('daysoverdue'))
+          });
+
+          acc.addresses = acc.addresses || [];
+          acc.addresses.push({
+            id: result.getValue({ name: 'internalid', join: 'address' }),
+            label: result.getValue({ name: 'addresslabel', join: 'address' })
+          });
+
+          return acc;
+        }, {});
+      }
 
       function getCustomers(searchText) {
         var results = search.create({
