593cb6fcd0e6a3117d7399e365ae429f9db9474f
Michael Robbins <mdrobbins@mdrnetworks.com>
2019-06-20 08:27:34 -0400

    Update suitelet to use file links rather than embedding content

--- backend/order-entry.suitelet.js
+++ backend/order-entry.suitelet.js
@@ -17,38 +17,41 @@ define(['N/file', 'N/search', 'N/url', 'N/config'],
         var companyName = companyPreferences.getValue('companyname');
 
         // noinspection CheckTagEmptyBody
-        var html = '<!doctype html>' +
+        var html = '<!doctype html>\n' +
           '   <html lang="en">' +
-          '       <head>' +
-          '           <meta charset="utf-8">' +
-          '           <script>' +
-          '               window.apiEndpoint = "' + apiEndpoint + '";' +
-          '               window.nsCompanyName = "' + companyName + '";' +
-          '           </script>' +
-          '           <title>Order Entry</title>' +
-          '       </head>' +
-          '       <body>' +
-          '       <style>' + getFileContents('order-entry.css') + '</style>' +
-          '           <div id="root"></div>' +
-          '       </body>' +
-          '       <script>' + getFileContents('order-entry.js') + '</script>' +
-          '   </html>';
+          '       <head>\n' +
+          '           <meta charset="utf-8">\n' +
+          '           <script>\n' +
+          '               window.apiEndpoint = "' + apiEndpoint + '";\n' +
+          '               window.nsCompanyName = "' + companyName + '";\n' +
+          '           </script>\n' +
+          '           <link type="text/css" rel="stylesheet" href="' + getFileUrl('order-entry.css') + '"/>\n' +
+          '           <title>Order Entry</title>\n' +
+          '       </head>\n' +
+          '       <body>\n' +
+          '           <div id="root"></div>\n' +
+          '       </body>\n' +
+          '       <script type="text/javascript" src="' + getFileUrl('order-entry.js') + '"></script>\n' +
+          '   </html>\n';
 
         context.response.write(html);
 
         //////////////////////////////////////////////////////////
 
-        function getFileContents(filename) {
-          var files = search.create({
+        function getFileUrl(filename) {
+          var results = search.create({
             type: 'file',
             filters: [
               ['name', 'is', filename]
+            ],
+            columns: [
+              'url'
             ]
           }).run().getRange({ start: 0, end: 1 });
 
-          return (files || []).reduce(function (contents, fileRecord) {
-            return file.load({ id: fileRecord.id }).getContents();
-          }, '');
+          if (Array.isArray(results) && results.length > 0) {
+            return results[0].getValue('url');
+          }
         }
       }
     };
